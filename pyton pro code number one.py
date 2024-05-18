ba209764140734eb2c63a22135a5ac434606a9a5
djgames12 <dantejaredpulido@gmail.com>
2024-05-18 10:20:22 -0600

    Add files via upload

---
+++ pyton pro code number one.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "XD" : "Una expresión para sonreir", 
+            "AFK" : "Away from keyboard - Significa que no estoy en el compu! ", 
+            "MINECRAFT" : " Juego Online de cubos , 15 años de Minecraft! ",
+            "IDK" :"No sé",
+            "POV" :"Point of view "
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+print(word)
